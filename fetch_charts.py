8c69f9be0a8676357286c7a42905dab148b3c6d2
Malcolm Aridory <aridoryjr@icloud.com>
2025-03-08 13:40:57 +0000

    feat(fetch_charts.py): turned charts fetch into a function with time, source and target parameters

--- fetch_charts.py
+++ fetch_charts.py
@@ -1,11 +1,15 @@
 import os
+from pathlib import Path
 import requests
 import pandas as pd
 from io import StringIO
-from datetime import date
+from datetime import date, datetime, timedelta
 from dateutil.rrule import rrule, DAILY
 import psycopg2
 import sqlalchemy as sa
+import urllib3
+
+urllib3.disable_warnings()
 
 # Connection parameters
 db_config = {
@@ -16,19 +20,45 @@ db_config = {
     "password": "postgres"  
 }
 
-connection_string = f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
-engine = sa.create_engine(connection_string)
+    
+def fetch_kworb_charts(source: str, target: str = 'sql', start: date = None, end: date = None):
+    start = datetime.today() - timedelta(days=1) if not start or start >= datetime.today() else start
+    end = datetime.today() - timedelta(days=1) if not end or end >= datetime.today() else end
+    
+    match source:
+        case 'apple':
+            resource_path = 'apple_songs/archive'
+        case 'itunes':
+            resource_path = 'ww/archive'
+        case 'radio':
+            resource_path = 'radio/archives'
+        case _:
+            raise ValueError(f"Source {source} not found.")
+    
+    match target:
+        case 'csv':
+            save_path = f"data/charts/{source}"
+            Path(save_path).mkdir(parents=True, exist_ok=True)
+            def save_data(dt: date, df: pd.DataFrame):
+                df.to_csv(f"{save_path}/{dt}.csv")  
+                print(f"Inserted data from {dt} into {save_path}/{dt}.csv")  
+        case 'sql':
+            connection_string = f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
+            engine = sa.create_engine(connection_string) 
+            def save_data(dt: date, df: pd.DataFrame):
+                html_tables.to_sql(f'chart_data_{source}', engine, if_exists="replace", index=False)
+                print(f"Inserted data from {dt} into chart_data_{source}")
+        case _:
+            raise ValueError(f"Target {target} is invalid.")
+                      
+
+    for current_date in rrule(DAILY, dtstart=start, until=end):
+        dt = current_date.strftime("%Y%m%d")
+        with requests.get(f'http://www.kworb.net/{resource_path}/{dt}.html', verify=False) as response:
+            html_tables = pd.read_html(StringIO(response.text))[0]
+            html_tables['date'] = current_date
+            save_data(dt, html_tables)
 
 
-path = os.path.dirname(__file__)
-start_date = date(2024, 1, 1)
-end_date = date(2024, 1, 5)
-
-for current_date in rrule(DAILY, dtstart=start_date, until=end_date):
-    dt = current_date.strftime("%Y%m%d")
-    with requests.get(f'http://www.kworb.net/apple_songs/archive/{dt}.html', verify=False) as response:
-        html_tables = pd.read_html(StringIO(response.text))[0]
-        html_tables['date'] = current_date
-        html_tables.to_csv(f'{path}/charts/apple/{dt}.csv')
-        html_tables.to_sql('chart_data', engine, if_exists="replace", index=False)
-        print(f"Inserted data from {dt} into {'chart_data'}")
+fetch_kworb_charts('apple', 'csv')
+fetch_kworb_charts('itunes', 'sql', start=datetime(year=2025, month=3, day=1))
