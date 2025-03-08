77f229849d4640b74208d935cb98fd5874d420b0
Malcolm Aridory <aridoryjr@icloud.com>
2025-03-08 14:31:08 +0000

    doc(fetch_charts.py): comments explaining script's functionment

--- fetch_charts.py
+++ fetch_charts.py
@@ -9,6 +9,7 @@ import psycopg2
 import sqlalchemy as sa
 import urllib3
 
+# Disables tls warnings in the console when fetching data
 urllib3.disable_warnings()
 
 # Connection parameters
@@ -22,9 +23,11 @@ db_config = {
 
     
 def fetch_kworb_charts(source: str, target: str = 'sql', start: date = None, end: date = None):
+    # start and end are yesterday by default to fetch the latest data
     start = datetime.today() - timedelta(days=1) if not start or start >= datetime.today() else start
     end = datetime.today() - timedelta(days=1) if not end or end >= datetime.today() else end
     
+    # match sources to their actual paths on kworb.net
     match source:
         case 'apple':
             resource_path = 'apple_songs/archive'
@@ -35,9 +38,11 @@ def fetch_kworb_charts(source: str, target: str = 'sql', start: date = None, end
         case _:
             raise ValueError(f"Source {source} not found.")
     
+    # matches our target parameter to either save as csv or in our db
     match target:
         case 'csv':
-            save_path = f"data/charts/{source}"
+            # create the path if it doesn't exist and create save_data fn to save the csv there
+            save_path = f"../data/charts/{source}"
             Path(save_path).mkdir(parents=True, exist_ok=True)
             def save_data(dt: date, df: pd.DataFrame):
                 df.to_csv(f"{save_path}/{dt}.csv")  
@@ -51,7 +56,7 @@ def fetch_kworb_charts(source: str, target: str = 'sql', start: date = None, end
         case _:
             raise ValueError(f"Target {target} is invalid.")
                       
-
+    # fetch and save each date between start date and end date
     for current_date in rrule(DAILY, dtstart=start, until=end):
         dt = current_date.strftime("%Y%m%d")
         with requests.get(f'http://www.kworb.net/{resource_path}/{dt}.html', verify=False) as response:
@@ -60,5 +65,6 @@ def fetch_kworb_charts(source: str, target: str = 'sql', start: date = None, end
             save_data(dt, html_tables)
 
 
-fetch_kworb_charts('apple', 'csv')
-fetch_kworb_charts('itunes', 'sql', start=datetime(year=2025, month=3, day=1))
+# example usages
+fetch_kworb_charts('apple', 'csv') # saves yesterday's apple music charts to a local csv file
+fetch_kworb_charts('itunes', 'sql', start=datetime(year=2025, month=3, day=1)) # saves itunes charts from 2025/03/01 to today in the db
