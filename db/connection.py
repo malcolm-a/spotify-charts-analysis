fc229c8dcd491ced21e26a96b84d178c2f226b16
malcolm-a <aridoryjr@icloud.com>
2025-03-08 20:43:20 +0100

    fix: no more conflicts with local postres

--- db/connection.py
+++ db/connection.py
@@ -1,15 +1,16 @@
 import sqlalchemy as sa
-
 db_config = {
     "host": "localhost", 
-    "port": 5432, 
+    "port": 5433,  # Changed to match Docker's exposed port
     "database": "postgres",  
     "user": "postgres", 
-    "password": "postgres"  
+    "password": "postgres",
+    "connect_timeout": 5,
+    "application_name": "docker_postgres"  # Help identify the connection
 }
 
 connection_string = f"postgresql://{db_config['user']}:{db_config['password']}@{db_config['host']}:{db_config['port']}/{db_config['database']}"
-engine = sa.create_engine(connection_string) 
+engine = sa.create_engine(connection_string)
 
 def get_engine():
     return engine
